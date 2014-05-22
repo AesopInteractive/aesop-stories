e972772a67d4038c9f320433756cdf2acdb2c15d
Bearded Avenger <email@nickhaskins.com>
2014-05-22 17:00:00 -0500

    basic cover js

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -1,22 +1,35 @@
-(function ( $ ) {
-	"use strict";
+jQuery(document).ready(function(){
 
-	$(function () {
-		// globa variabls
-		var windowWidth = $(window).width(),
-			windowHeight = $(window).height(),
-			storyHeader	= $('.aesop-story-header');
+	// globa variabls
+	var storyHeader	= jQuery('.aesop-story-header'),
+		storyEntry  = jQuery('.aesop-story-entry');
 
-		//  global functions
-		var storyResizer = function(){
-    		$(storyHeader).css({'height':windowHeight+'px'});
-    	}
+	//  global functions
+	var storyResizer = function(){
+		jQuery(storyHeader).css({'height':(jQuery(window).height())+'px', 'width':(jQuery(window).width())+'px'});
+		jQuery(storyEntry).css({'margin-top':(jQuery(window).height())+'px'});
+	}
 
-    	storyResizer();
+	storyResizer();
 
-    	jQuery(window).resize(function(){
-	        storyResizer();
-	    });
+	jQuery(window).resize(function(){
+        storyResizer();
+    });
+
+	// when the story hits the bottom page
+	jQuery(storyEntry).waypoint(function(direction) {
+
+		// toggle a class of faded so we can fade a white mask in and out
+	   	jQuery(storyHeader).toggleClass('faded');
+
+	}, {offset:'100%'});
+
+	// when the story hits the top of the page
+	jQuery(storyEntry).waypoint(function(direction) {
+
+		// remove the story cover so it doesnt bleed out the bottom
+	   	jQuery(storyHeader).toggleClass('not-visible');
+
 	});
 
-}(jQuery));
+});
