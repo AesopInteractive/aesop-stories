5d5eaa86d474d7a4b7e8f0bb229c3554f8c85a94
Bearded Avenger <email@nickhaskins.com>
2014-05-26 09:22:35 -0500

    story header effects

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -50,4 +50,43 @@ jQuery(document).ready(function(){
 
 	});
 
+	// fancy hidden nav
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = jQuery('.aesop-story-header').outerHeight();
+
+	jQuery(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = jQuery(this).scrollTop();
+
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        jQuery('.aesop-story-header').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	        // Scroll Up
+	        if(st + jQuery(window).height() < jQuery(document).height()) {
+	            jQuery('.aesop-story-header').removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+
+	    lastScrollTop = st;
+	}
 });
