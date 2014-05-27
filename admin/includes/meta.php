610c0079586021a490aa954890074f041b42a406
Bearded Avenger <email@nickhaskins.com>
2014-05-27 17:14:25 -0500

    story cover controls

--- admin/includes/meta.php
+++ admin/includes/meta.php
@@ -9,6 +9,148 @@ class AesopStoriesMeta {
 	}
 
 	function meta( array $meta_boxes ) {
+
+
+			$opts = array(
+				array(
+					'id'             	=> 'aesop_stories_global_help',
+					'name'           	=> ' ',
+					'type'				=> 'title',
+					'cols'				=> 12,
+					'desc'				=> __('Use the controls below to craft the look of this specific story.','aesop_stories')
+				),
+				array(
+					'id'				=> 'aesop_stories_cover_text_color',
+					'name'				=> __('Cover Text Color (optional)', 'aesop_stories'),
+					'type'				=> 'colorpicker',
+					'cols'				=> 4
+				),
+				array(
+					'id'				=> 'aesop_stories_article_bg',
+					'name'				=> __('Story Background Color (optional)', 'aesop_stories'),
+					'type'				=> 'colorpicker',
+					'cols'				=> 4
+				),
+				array(
+					'id'				=> 'aesop_stories_article_text',
+					'name'				=> __('Story Text Color (optional)', 'aesop_stories'),
+					'type'				=> 'colorpicker',
+					'cols'				=> 4
+				),
+				array(
+					'id'				=> 'aesop_stories_article_mask_color',
+					'name'				=> __('Story Cover Mask Color (optional)', 'aesop_stories'),
+					'type'				=> 'colorpicker',
+					'cols'				=> 6
+				),
+				array(
+					'id'				=> 'aesop_stories_article_mask_opacity',
+					'name'				=> __('Story Cover Mask Opacity', 'aesop_stories'),
+					'type'           	=> 'select',
+					'default'			=> '0.6',
+					'options'			=> array(
+						'0.1'			=> __('0.1', 'aesop_stories'),
+						'0.2'			=> __('0.2', 'aesop_stories'),
+						'0.3'			=> __('0.3', 'aesop_stories'),
+						'0.4'			=> __('0.4', 'aesop_stories'),
+						'0.5'			=> __('0.5', 'aesop_stories'),
+						'0.6'			=> __('0.6', 'aesop_stories'),
+						'0.7'			=> __('0.7', 'aesop_stories'),
+						'0.8'			=> __('0.8', 'aesop_stories'),
+						'0.9'			=> __('0.9', 'aesop_stories'),
+						'1.0'			=> __('1.0', 'aesop_stories')
+					),
+					'cols'				=> 6
+				),
+				array(
+					'id'             	=> 'aesop_stories_block_cover_options',
+					'name'           	=> __('Block Style Cover Options', 'aesop_stories'),
+					'type'				=> 'title'
+				),
+				array(
+					'id'             	=> 'aesop_stories_block_cover_options',
+					'name'           	=> ' ',
+					'type'				=> 'title',
+					'desc'				=> __('If you are using Block Style Cover option, and you do not like the way the title has been automatically formatted, you can specify the title and what lines the words are on using the fields below.', 'aesop_stories')
+				),
+				array(
+					'id' 			=> 'aesop_stories_cover_lines',
+					'name' 			=> __('Cover Lines', 'aesop-stories'),
+					'type' 			=> 'group',
+					'repeatable'     => true,
+					'repeatable_max' => 5,
+					'sortable'		=> true,
+					'desc'			=> __('Add a line for each text item.', 'aesop-stories'),
+					'fields' 		=> array(
+						array(
+							'id' 	=> 'text',
+							'name' 	=> __('Text', 'aesop-stories'),
+							'type' 	=> 'text',
+							'cols'	=> 6
+						)
+					)
+				),
+				/*
+				array(
+					'id'				=> 'aesop_stories_block_cover_line2',
+					'name'				=> __('Cover Line 2', 'aesop_stories'),
+					'type'				=> 'text',
+					'cols'				=> 4
+				),
+				array(
+					'id'				=> 'aesop_stories_block_cover_line3',
+					'name'				=> __('Cover Line 3', 'aesop_stories'),
+					'type'				=> 'text',
+					'cols'				=> 4
+				),
+				array(
+					'id'				=> 'aesop_stories_block_cover_line4',
+					'name'				=> __('Cover Line 4', 'aesop_stories'),
+					'type'				=> 'text',
+					'cols'				=> 4
+				),
+				array(
+					'id'				=> 'aesop_stories_block_cover_line5',
+					'name'				=> __('Cover Line 5', 'aesop_stories'),
+					'type'				=> 'text',
+					'cols'				=> 4
+				),
+				array(
+					'id'				=> 'aesop_stories_block_cover_line5',
+					'name'				=> '',
+					'type'				=> 'title',
+					'cols'				=> 4,
+				),
+				*/
+				array(
+					'id'             	=> 'aesop_stories_block_cover_options',
+					'name'           	=> ' ',
+					'type'				=> 'title',
+					'desc'				=> __('The two options below control the appearence of the cover title. By default, the maximum font size is <code>400</code>. If you have a really short title, try making this number larger, like <code>600</code>. If you have a really long title, make this number shorter like <code>200</code>. You can use this together with the maximum title width. By default, it\'s set at <code>60%</code>. However you can change this to something like <code>75%</code>', 'aesop_stories')
+				),
+				array(
+					'id'				=> 'aesop_stories_block_title_size',
+					'name'				=> __('Maximum Font Size', 'aesop_stories'),
+					'type'				=> 'text',
+					'default'			=> 400,
+					'cols'				=> 6
+				),
+				array(
+					'id'				=> 'aesop_stories_block_title_width',
+					'name'				=> __('Maximum Title Width', 'aesop_stories'),
+					'type'				=> 'text',
+					'default'			=> '70%',
+					'cols'				=> 6
+				),
+			);
+
+			$meta_boxes[] = array(
+				'title' => __('Story Cover', 'aesop_stories-core'),
+				'pages' 	=> array('aesop_stories'),
+				'fields' => $opts
+			);
+
+
 		$meta_boxes[] = array(
 			'title' => __('Aesop Stories', 'aesop-stories'),
 			'pages' 	=> array('aesop_stories'),
--- public/includes/styles.php
+++ public/includes/styles.php
@@ -2,22 +2,76 @@
 
 class AesopStoriesStyles {
 
-	function __construct(){
-		//add_action('wp_head',array($this,'styles'));
+	public function __construct(){
+		add_action('wp_head', array($this,'story_styles'));
 	}
 
-	function styles(){
-
+	public function story_styles(){
 		global $post;
 
-		//$width 		= aesop_stories_get_opt( 'aesop_stories_width', 'aesop_story_settings_design' );
+		$maxfontsize    = get_post_meta(get_the_ID(),'aesop_stories_block_title_size', true) ? get_post_meta(get_the_ID(),'aesop_stories_block_title_size', true) : 400;
+		$covertype		= get_post_meta(get_the_ID(),'aesop_stories_article_cover_type', true) ? get_post_meta(get_the_ID(),'aesop_stories_article_cover_type', true) : 'default-cover';
 
-	    if ( 'aesop_stories' == get_post_type() || is_post_type_archive('aesop_stories') ) { ?>
-	    	<!-- Aesop Story Styles -->
-	    	<style>
+		$coverline1     = get_post_meta(get_the_ID(),'aesop_stories_block_cover_line1', true);
+		$coverline2     = get_post_meta(get_the_ID(),'aesop_stories_block_cover_line2', true);
+		$coverline3     = get_post_meta(get_the_ID(),'aesop_stories_block_cover_line3', true);
+		$coverline4     = get_post_meta(get_the_ID(),'aesop_stories_block_cover_line4', true);
+		$coverline5     = get_post_meta(get_the_ID(),'aesop_stories_block_cover_line5', true);
 
-	    	</style>
-    	<?php }
+		$coverlines = get_post_meta( get_the_ID(), 'aesop_stories_cover_lines', false);
+
+		$titlecolor     = get_post_meta(get_the_ID(),'aesop_stories_cover_text_color', true);
+
+		$titlewidth  	= get_post_meta(get_the_ID(),'aesop_stories_block_title_width', true);
+
+		$coverwidthstyle = $titlewidth ? sprintf('style="width:%s;"',$titlewidth) : false;
+
+
+		$opts = get_option('aesop_stories_options') ? get_option('aesop_stories_options') : false;
+    	$defaultbg = isset($opts['bg']) ? $opts['bg'] : '#FFFFFF';
+
+    	$storybasecolor = get_post_meta(get_the_ID(),'aesop_stories_article_bg', true);
+		$storytxtcolor = get_post_meta(get_the_ID(),'aesop_stories_article_text', true);
+
+
+		if($coverline1) { ?>
+			<!-- Story Cover Slabtext -->
+	    	<script>
+				jQuery(document).ready(function(){
+					stS = "<span class='slabtext'>";
+				    stE = "</span>";
+					txt = [<?php foreach($coverlines as $coverline) { ?>"<?php echo $coverline['text'];?>",<?php;}?>];
+
+					jQuery('.aesop-story-cover .aesop-story-title').html(stS + txt.join(stE + stS) + stE).slabText({maxFontSize:<?php echo $maxfontsize;?>});
+				});
+			</script>
+		<?php } else { ?>
+			<!-- Story Cover Slabtext -->
+			<script>
+				jQuery(document).ready(function(){
+					jQuery('.aesop-story-cover .aesop-story-title').slabText({maxFontSize:<?php echo $maxfontsize;?>});
+				});
+			</script>
+		<?php }
+
+		if ($titlewidth) { ?>
+			<!-- Story Title Width Styles -->
+			<style>
+				.postid-<?php echo get_the_ID();?> .block-cover .aesop_stories-cover-title {
+					width:<?php echo $titlewidth;?>;
+				}
+			</style>
+		<?php }
+
 	}
+
 }
 new AesopStoriesStyles;
+
+
+
+
+
+
+
+
