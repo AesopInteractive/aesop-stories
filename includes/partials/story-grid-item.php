7432b4adf7c6ef45570761d31e1cbffad078930a
Bearded Avenger <email@nickhaskins.com>
2014-06-30 15:36:39 -0500

    excerpt fixes in grid

--- includes/partials/story-grid-item.php
+++ includes/partials/story-grid-item.php
@@ -1,10 +1,10 @@
 <figure class="aesop-stories-grid-item">
 
-	<?php the_post_thumbnail( get_the_ID(), 'aesop-story-grid');?>
+	<?php the_post_thumbnail();?>
 
 	<figcaption>
 		<?php the_title('<h2>','</h2>');?>
-		<?php the_excerpt();?>
+		<p><?php echo wp_trim_words(get_the_excerpt(), 5, '...');?></p>
 		<a href="<?php echo the_permalink();?>"><?php echo apply_filters('aesop_stories_read_more',_e('Read More','aesop-stories'));?></a>
 	</figcaption>
 
